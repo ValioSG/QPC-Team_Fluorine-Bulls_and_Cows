c9149236b3e66e46f95224cb4b3d93ee7a6d37b1
Kris Nickson <absolutepowerforce_zx@yahoo.com>
2013-05-17 15:00:33 +0300

    Added tests

---
+++ CowsAndBullsUnitTests/PlayerScoreTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CowsAndBulls;
+
+namespace CowsAndBullsUnitTests
+{
+    [TestClass]
+    public class PlayerScoreTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            PlayerScore testPlayer = new PlayerScore("Looser", 12739812);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorTest_EmptyName()
+        {
+            PlayerScore testPlayer = new PlayerScore("", 12739812);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorTest_NullName()
+        {
+            PlayerScore testPlayer = new PlayerScore(null, 12739812);
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            string expectedString = "Pesho --> 12 guesses";
+            PlayerScore testPlayer = new PlayerScore("Pesho", 12);
+            Assert.AreEqual(expectedString, testPlayer.ToString());
+        }
+    }
+}
---
+++ CowsAndBullsUnitTests/ScoreBoardTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using CowsAndBulls;
+using System.Collections.Generic;
+
+
+namespace CowsAndBullsUnitTests
+{
+    [TestClass]
+    public class ScoreBoardTests
+    {
+        private string fileName = "testFile.txt";
+
+        [TestInitialize]
+        public void createFile()
+        {
+            if (File.Exists(fileName) == false)
+            {
+                File.Create(fileName).Close();
+            }
+
+        }
+
+        [TestMethod]
+        public void TestConstructor_CorrectFileName()
+        {
+            //createFile();
+            Scoreboard testBoard = new Scoreboard(fileName);
+        }
+
+        [TestMethod]
+        public void TestAddAndSave()
+        {
+            //createFile();
+
+            Scoreboard testBoard = new Scoreboard(fileName);
+            testBoard.AddScore("lolminator", 1337);
+            testBoard.AddScore("asdf", 1337);
+            testBoard.AddScore("3rqwe", 1337);
+
+            testBoard.SaveToFile();
+        }
+
+        [TestMethod]
+        public void TestRead()
+        {
+            //createFile();
+            Scoreboard testBoard = new Scoreboard(fileName);
+            testBoard.AddScore("qweqwe", 1337);
+            testBoard.AddScore("gtegrge", 12);
+            testBoard.AddScore("rwetwehhg", 18);
+
+            //testBoard.
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TestIncorrectFileName_FileNotFound()
+        {
+            Scoreboard testBoard = new Scoreboard("aoiheiqwhe");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestIncorrectFileName_NullPath()
+        {
+            Scoreboard testBoard = new Scoreboard(null);
+        }
+    }
+}
